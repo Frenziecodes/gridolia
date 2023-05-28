a68db79915b873e8d4233b757c9d8973ff41b42c
Lewis ushindi <coderflame3@gmail.com>
2023-05-28 16:26:32 +0300

    adding modal structure

--- src/sections/GridGenerator.jsx
+++ src/sections/GridGenerator.jsx
@@ -1,13 +1,16 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { Responsive, WidthProvider } from 'react-grid-layout';
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
+import copy from 'copy-to-clipboard';
 
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
 const GridGenerator = () => {
   const [layout, setLayout] = useState([]);
   const [gridItems, setGridItems] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [generatedCode, setGeneratedCode] = useState('');
 
   const handleLayoutChange = (newLayout) => {
     setLayout(newLayout);
@@ -38,16 +41,49 @@ const GridGenerator = () => {
   };
 
   const handleGenerateLayout = () => {
-    const code = `grid-template-columns: repeat(${layout[0].w}, 1fr);
-grid-template-rows: repeat(${layout[0].h}, 1fr);`;
+    const gridTemplateColumns = `repeat(${layout[0].w}, 1fr)`;
+    const gridTemplateRows = `repeat(${layout[0].h}, 1fr)`;
 
-    alert(code);
+    const htmlCode = `
+<div class="grid-container">
+  ${gridItems
+    .map(
+      (item) => `<div class="grid-item">${item.label || item.i}</div>`
+    )
+    .join('\n')}
+</div>
+`;
+
+    const cssCode = `
+.grid-container {
+  display: grid;
+  grid-template-columns: ${gridTemplateColumns};
+  grid-template-rows: ${gridTemplateRows};
+}
+
+.grid-item {
+  /* Add styling for grid items */
+}
+`;
+
+    setGeneratedCode(htmlCode + cssCode);
+    setShowModal(true);
   };
+
+  const handleCopyCode = () => {
+    copy(generatedCode);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  const codeTextAreaRef = useRef(null);
 
   return (
     <div className="container">
       <div className="flex  min-h-screen">
-        <div className='shadow-2xl bg-teal-900 p-3 w-72'>
+        <div className="shadow-2xl bg-teal-900 p-3 w-72">
           <h1 className="text-2xl text-white font-bold mb-4">GRIDOLIA</h1>
           <div className="mb-4">
             <button
@@ -89,7 +125,7 @@ grid-template-rows: repeat(${layout[0].h}, 1fr);`;
           </div>
         </div>
 
-        <div className='bg-teal-100 flex-grow'>
+        <div className="bg-teal-100 flex-grow">
           <ResponsiveGridLayout
             className="layout"
             layouts={{ lg: layout }}
@@ -110,6 +146,34 @@ grid-template-rows: repeat(${layout[0].h}, 1fr);`;
           </ResponsiveGridLayout>
         </div>
       </div>
+
+      {showModal && (
+        <div className="fixed inset-0 bg-gray-900 bg-opacity-75 flex items-center justify-center">
+          <div className="bg-white p-4 rounded shadow">
+            <textarea
+              ref={codeTextAreaRef}
+              className="w-full h-40 border border-gray-300 rounded px-2 py-1 mb-4"
+              value={generatedCode}
+              readOnly
+            />
+
+            <div className="flex justify-end">
+              <button
+                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mr-2"
+                onClick={handleCopyCode}
+              >
+                Copy Code
+              </button>
+              <button
+                className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600"
+                onClick={handleCloseModal}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
