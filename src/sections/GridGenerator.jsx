fce467a3706c6d1d669a50ea0bcadb535cfb6f0c
Lewis ushindi <coderflame3@gmail.com>
2023-05-29 10:55:07 +0300

    update ui styling

--- src/sections/GridGenerator.jsx
+++ src/sections/GridGenerator.jsx
@@ -23,14 +23,14 @@ const Gridiola = () => {
   };
 
   return (
-    <div className="flex">
-      <div className="w-1/2 p-4">
-        <h2 className="text-2xl font-bold mb-4">Grid Configuration</h2>
+    <div className="flex min-h-screen">
+      <div className="w-72 bg-teal-600 p-4">
+        <h2 className="text-2xl font-bold mb-4">Gridiola</h2>
         <div className="mb-4">
           <label className="block font-medium mb-1">Number of Rows</label>
           <input
             type="number"
-            className="w-full rounded border-gray-300"
+            className="w-full rounded border-gray-300 px-2 py-2"
             value={numRows}
             onChange={(e) => setNumRows(parseInt(e.target.value))}
           />
@@ -39,7 +39,7 @@ const Gridiola = () => {
           <label className="block font-medium mb-1">Number of Columns</label>
           <input
             type="number"
-            className="w-full rounded border-gray-300"
+            className="w-full rounded border-gray-300 px-2 py-2"
             value={numColumns}
             onChange={(e) => setNumColumns(parseInt(e.target.value))}
           />
@@ -48,7 +48,7 @@ const Gridiola = () => {
           <label className="block font-medium mb-1">Row Height</label>
           <input
             type="number"
-            className="w-full rounded border-gray-300"
+            className="w-full rounded border-gray-300 px-2 py-2"
             value={rowHeight}
             onChange={(e) => setRowHeight(parseInt(e.target.value))}
           />
@@ -57,7 +57,7 @@ const Gridiola = () => {
           <label className="block font-medium mb-1">Column Width</label>
           <input
             type="number"
-            className="w-full rounded border-gray-300"
+            className="w-full rounded border-gray-300 px-2 py-2"
             value={columnWidth}
             onChange={(e) => setColumnWidth(parseInt(e.target.value))}
           />
@@ -66,21 +66,21 @@ const Gridiola = () => {
           <label className="block font-medium mb-1">Row Gap</label>
           <input
             type="number"
-            className="w-full rounded border-gray-300"
+            className="w-full rounded border-gray-300 px-2 py-2"
             value={rowGap}
             onChange={(e) => setRowGap(parseInt(e.target.value))}
           />
         </div>
         <button
-          className="bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded"
+          className="bg-teal-900 hover:bg-teal-800 text-white font-medium py-2 px-4 rounded"
           onClick={handleGridPropertiesChange}
         >
           Generate Grid
         </button>
       </div>
 
-      <div className="w-1/2 p-4">
-        <h2 className="text-2xl font-bold mb-4">Grid Layout Preview</h2>
+      <div className="w-full p-4 bg-teal-100">
+        <h2 className="text-2xl font-bold mb-4 ">Grid Layout Preview</h2>
         <div
           className="grid"
           style={{
@@ -92,7 +92,7 @@ const Gridiola = () => {
           {gridLayout.map((item, index) => (
             <div
               key={index}
-              className="bg-gray-200 border border-gray-300 flex justify-center items-center"
+              className="bg-white border border-gray-300 flex justify-center items-center"
             >
               {`${item.row + 1}, ${item.column + 1}`}
             </div>
